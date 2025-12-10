c667730728c2db52a15df876fac2c9a7185d246d
Alexander Lapod <alexanderfabian18@gmail.com>
2025-12-10 09:21:38 +0700

    Making home dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'home',
     pathMatch: 'full',
   },
   {
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { IonicModule } from '@ionic/angular';   
+import { Router, RouterLink } from '@angular/router'; 
+import { AuthService } from '../services/auth';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonicModule, CommonModule, RouterLink], 
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  
+  user: any = {};
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ionViewWillEnter() {
+    this.user = this.authService.getUser();
+    
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigateByUrl('/login');
+    }
+  }
+
+  ngOnInit() {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
 }
